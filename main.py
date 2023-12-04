1db9f93890d57b655b6d44c721136ff1afc2ce28
aadharkaushik <aadharkaushik37@gmail.com>
2023-12-04 18:27:40 +0530

    Adding Pages and Underlining the Heading.

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="B", size=12)
+for index,row in df.iterrows():
+    pdf.add_page()
 
-pdf.cell(w=0, h=12, txt="Hi King", align="L", ln=1, border=1)
-
-pdf.set_font(family="Helvetica", style="I", size=12)
-
-pdf.cell(w=0, h=12, txt="Hi Sir", align="R", ln=1, border=1)
-
-pdf.set_font(family="Arial", style="B", size=12)
-
-pdf.cell(w=0, h=12, txt="Hello World", align="C", ln=1, border=1)
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(10, 20, 200, 20)
 
 pdf.output("output.pdf")
